a686bee49a5e2142adabd0807e52889e2c935214
“acse-a459408a” <s0919590354@gmail.com>
2022-10-18 14:53:43 +0100

    modify flake8

--- simple_functions/functions1.py
+++ simple_functions/functions1.py
@@ -1,12 +1,16 @@
 from functools import cache
-__all__ = ['my_sum','factorial']
+__all__ = ['my_sum', 'factorial']
 
-# ccc#
+# ccc
+
+
 def my_sum(iterable):
     tot = 0
     for i in iterable:
         tot += i
     return tot
+
+
 @cache
 def factorial(n):
     return n * factorial(n-1) if n else 1
